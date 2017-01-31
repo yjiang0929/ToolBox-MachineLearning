b98b5fa27928280f0e6e177bde0c62583faebb96
Serena Chen <serenazychen@gmail.com>
2017-01-30 19:50:11 -0500

    finished updating the toolbox

--- learning_curve.py
+++ learning_curve.py
@@ -6,23 +6,41 @@ from sklearn.datasets import *
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-data = load_digits()
-print(data.DESCR)
-num_trials = 10
-train_percentages = range(5, 95, 5)
-test_accuracies = numpy.zeros(len(train_percentages))
 
-# train a model with training percentages between 5 and 90 (see
-# train_percentages) and evaluate the resultant accuracy.
-# You should repeat each training percentage num_trials times to smooth out
-# variability
-# for consistency with the previous example use
-# model = LogisticRegression(C=10**-10) for your learner
+def display_digits():
+    digits = load_digits()
+    print(digits.DESCR)
+    fig = plt.figure()
+    for i in range(10):
+        subplot = fig.add_subplot(5, 2, i+1)
+        subplot.matshow(numpy.reshape(digits.data[i], (8, 8)), cmap='gray')
 
-# TODO: your code here
+    plt.show()
 
-fig = plt.figure()
-plt.plot(train_percentages, test_accuracies)
-plt.xlabel('Percentage of Data Used for Training')
-plt.ylabel('Accuracy on Test Set')
-plt.show()
+
+def train_model():
+    data = load_digits()
+    num_trials = 10
+    train_percentages = range(5, 95, 5)
+    test_accuracies = numpy.zeros(len(train_percentages))
+
+    # train models with training percentages between 5 and 90 (see
+    # train_percentages) and evaluate the resultant accuracy for each.
+    # You should repeat each training percentage num_trials times to smooth out
+    # variability.
+    # For consistency with the previous example use
+    # model = LogisticRegression(C=10**-10) for your learner
+
+    # TODO: your code here
+
+    fig = plt.figure()
+    plt.plot(train_percentages, test_accuracies)
+    plt.xlabel('Percentage of Data Used for Training')
+    plt.ylabel('Accuracy on Test Set')
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Feel free to comment/uncomment as needed
+    display_digits()
+    # train_model()
